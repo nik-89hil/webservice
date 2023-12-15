866906d195a2d4c8c3c504f780a0a3828dcb8ee7
Nitin Kumar <nikhilkumar19072002@gmail.com>
2023-12-15 17:47:23 +0530

    seond commit

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   
     <BrowserRouter>
     <Routes>
-      <Route path='/'  element={<Home/>}/>
+      <Route exact path='/'  element={<Home/>}/>
       
     </Routes>
     </BrowserRouter>
